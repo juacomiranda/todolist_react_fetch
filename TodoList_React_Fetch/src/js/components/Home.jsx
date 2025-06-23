494e79483d755e23eaa8147cc9156cb0553c2ce5
juacomiranda <jmiranda@live.cl>
2025-06-23 08:52:12 -0400

    primer commit

---
+++ TodoList_React_Fetch/src/js/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import List from ""
+
+const Home = () => {
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">TODO LIST FETCH</h1>
+			<div>
+				<List />
+			</div>
+		</div>
+	);
+};
+
+export default Home;
---
+++ TodoList_React_Fetch/src/js/components/lista.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function List () {
+    return (
+        <div className=""></div>
+    );
+    
+}
